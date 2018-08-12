73b3a0aecbb5e27786cc45a4de004ed6c04923d5
Ryan Ward <rkw6086@ryans-mbp.lan>
2018-08-11 19:44:10 -0500

    Allowing for cross platform API calls

--- app/config/environments/standalone.js
+++ app/config/environments/standalone.js
@@ -1,6 +1,15 @@
+import { Platform } from 'react-native';
+
 const Environment = {
     api: {
-        baseUrl: "http://localhost:5000/api/"
+        ...Platform.select({
+            ios: {
+                baseUrl: "http://localhost:5000/api/"
+            },
+            android: {
+                baseUrl: "http://10.0.2.2:5000/api/"
+            }
+        })
     },
     localStorageKeys: {
         token: 'GOGOYUM::AUTH_TOKEN'
