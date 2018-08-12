0386f720ddd44a1028e0ed92f0b1d78e6e20bc22
Ryan Ward <rkw6086@ryans-mbp.lan>
2018-08-12 12:20:28 -0500

    Cleanup.

--- app/scenes/dashboard/tile.js
+++ app/scenes/dashboard/tile.js
@@ -4,7 +4,8 @@ import PropTypes from 'prop-types';
 
 const styles = StyleSheet.create({
     sharedContainerStyle: {
-        alignContent: 'center'
+        justifyContent: 'center',
+        alignItems: 'center'
     },
     darkGreenContainer: {
         flex: 1,
